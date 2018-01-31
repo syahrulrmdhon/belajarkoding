59dc3d7fe96b48525450045daa68a7fd00ba59ce
syahrulrmdhon <syahrulrmdhon2@gmail.com>
2018-01-31 14:53:08 +0700

    unsubscribe sudah

--- wp-content/plugins/syahrul-list-builder/js/public/syahrul-list-builder.js
+++ wp-content/plugins/syahrul-list-builder/js/public/syahrul-list-builder.js
@@ -16,12 +16,14 @@ jQuery(document).ready(function ($) {
 				if( data.status == 1 ) {
 					$form[0].reset();
 					alert(data.message);
+          console.log(form_data);
 				} else {
 					var msg = data.message + '\r' + data.error + '\r';
 					$.each(data.errors,function(key,value){
 						msg += '\r';
 						msg += '- '+ value;
 					});
+          console.log(form_data);
 					alert( msg );
 				}
 			},
@@ -38,44 +40,44 @@ jQuery(document).ready(function ($) {
 
 // email capture action url
 	var unsubscribe_url = wpajax_url + '?action=slb_unsubscribe';
+  $(document).on('submit','form#slb_manage_subscriptions_form',function(){
 
-	$(document).on('submit','form#slb_manage_subscriptions_form',function(){
+  		// get the jquery form object
+  		$form = $(this);
 
-		// get the jquery form object
-		$form = $(this);
+  		// setup our form data for our ajax post
+  		var form_data = $form.serialize();
 
-		// setup our form data for our ajax post
-		var form_data = $form.serialize();
+  		// submit our form data with ajax
+  		$.ajax({
+  			'method':'post',
+  			'url':unsubscribe_url,
+  			'data':form_data,
+  			'dataType':'json',
+        contentType: 'json',
+  			'cache':false,
+  			'success': function( data, textStatus ) {
+  				if( data.status == 1 ) {
+  					// success
+  					// update form html
+  					$form.replaceWith(data.html);
+  					// notify the user of success
+  					alert(data.message);
+  				} else {
+  					// error
+  					// begin building our error message text
+  					var msg = data.message + '\r' + data.error + '\r';
+  					alert(msg);
+  				}
+  			},
+  			'error': function( jqXHR, textStatus, errorThrown ) {
+  				// ajax didn't work
+  			}
 
-		// submit our form data with ajax
-		$.ajax({
-			'method':'post',
-			'url':wpajax_url + '?action=slb_unsubscribe',
-			'data':form_data,
-			'dataType':'json',
-			'cache':false,
-			'success': function( data, textStatus ) {
-				if( data.status == 1 ) {
-					// success
-					// update form html
-					$form.replaceWith(data.html);
-					// notify the user of success
-					alert(data.message);
-				} else {
-					// error
-					// begin building our error message text
-					var msg = data.message + '\r' + data.error + '\r';
-					alert(msg);
-				}
-			},
-			'error': function( jqXHR, textStatus, errorThrown ) {
-				// ajax didn't work
-			}
+  		});
 
-		});
+  		// stop the form from submitting normally
+  		return false;
 
-		// stop the form from submitting normally
-		return false;
-
-	});
+  	});
 });
