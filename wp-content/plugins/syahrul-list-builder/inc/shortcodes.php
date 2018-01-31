b34289ee1290c02eaa7c52b3148a3749d727839a
Syahrul Romadhon <syahrulrmdhon2@gmail.com>
2018-01-31 16:57:21 +0700

    balik

--- wp-content/plugins/syahrul-list-builder/inc/shortcodes.php
+++ wp-content/plugins/syahrul-list-builder/inc/shortcodes.php
@@ -60,7 +60,7 @@
 
       function test($args, $content=''){
         $a = slb_get_list_subscribers(0);
-        $b = print_r($a,true)."<br/><br/>";
+        $b = print_r($a,true);
         return $b;
       }
 
@@ -72,7 +72,7 @@
           $c[] = $a['email'];
           $i++;
         }
-        $b = "<h3>List Subscriber</h3> <br/>".print_r($c,true);
+        $b = print_r($c,true);
         return $b;
       }
 
